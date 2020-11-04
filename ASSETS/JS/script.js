ec73820ffbb3be80f1f95e8e32f706e909db539f
LaurenceB28 <laurence.brd@gmail.com>
2020-11-04 09:50:14 +0100

    exoJS6

---
+++ ASSETS/JS/script.js
@@ -0,0 +1,7 @@
+function calculer(){
+    var point = document.getElementById("pointure").value;
+    var year = document.getElementById("birthdate").value;
+    var result = (((((point*2)+5)*50)-year)+1769);
+
+    alert('Résultat : ' + result)
+    }
